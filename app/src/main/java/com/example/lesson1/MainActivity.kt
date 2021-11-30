5df672c200d7b461f456f67756fd73f246defb4e
stFrantic <emurashkin96@gmail.com>
2021-11-30 20:56:48 +0300

    Task 6-7

--- app/src/main/java/com/example/lesson1/MainActivity.kt
+++ app/src/main/java/com/example/lesson1/MainActivity.kt
@@ -1,24 +1,37 @@
 package com.example.lesson1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.lesson1.src.Character
 import fibonacci
-
 
 class MainActivity : AppCompatActivity() {
     private lateinit var submit: Button
-    private lateinit var answer: TextView
-    private lateinit var editText: EditText
+    private lateinit var count: Button
+    private lateinit var generate: Button
 
+    private lateinit var answer: TextView
+    private lateinit var taps: TextView
+    private lateinit var data: TextView
+
+
+
+    private lateinit var editText: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         submit = findViewById(R.id.button)
         answer = findViewById(R.id.textView)
 
+        count = findViewById(R.id.button2)
+        taps = findViewById(R.id.textView2)
+
+        generate = findViewById(R.id.generate)
+        data = findViewById(R.id.textView3)
         if (this::submit.isInitialized) {
             submit.setOnClickListener {
                 editText = findViewById(R.id.editText1)
@@ -28,8 +41,22 @@ class MainActivity : AppCompatActivity() {
                     // for (i in 0 until number + 1) result[i] = fibonacci(i)
                     //answer.text = "answer is ${result.contentToString()}"
                     answer.text = "Answer is ${fibonacci(number + 1).contentToString()}"
-                }else answer.text = "Number must be more then 0, and less then ${Int.MAX_VALUE - 1}"
+                } else answer.text =
+                    "Number must be more then 0, and less then ${Int.MAX_VALUE - 1}"
 
+            }
+        }
+        if (this::count.isInitialized) {
+            var counter: Int = 0;
+            count.setOnClickListener {
+                taps.text = "Amount of taps: ${++counter}"
+            }
+        }
+        if (this::generate.isInitialized) {
+            generate.setOnClickListener {
+                var ch = Character()
+                ch.createHero()
+                data.text = ch.toString()
             }
         }
 
